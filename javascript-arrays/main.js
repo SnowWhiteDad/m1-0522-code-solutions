7f747fa057d4a605a58ec6549dc36bf5c5ef06b2
Ola Sh2 <shittu.babs@gmail.com>
2022-06-01 04:54:09 +0000

    Completed javascript-arrays exercise.

---
+++ javascript-arrays/main.js
@@ -0,0 +1,33 @@
+var colors = ['red', 'white', 'blue'];
+
+console.log('first color: ', colors[0]);
+console.log('Second color: ', colors[1]);
+console.log('Third color: ', colors[2]);
+
+console.log('');
+
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]);
+
+colors[2] = 'green';
+
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2]);
+
+console.log('Value of colors: ', colors);
+
+var students = ['Jamiu Ibrahim', 'Aliko Dangote', 'Chong Hong', 'Wills Mountbarten'];
+var numberOfStudents = students.length;
+
+console.log('');
+
+console.log('There are ', numberOfStudents, ' students in the class.');
+
+var lastIndex = numberOfStudents - 1;
+var lastStudent = students[lastIndex];
+
+console.log('');
+
+console.log('The last student in the array is ', lastStudent);
+
+console.log('');
+
+console.log('Value of students:', students);
