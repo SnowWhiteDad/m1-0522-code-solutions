d7908ba35f781e2062f5e6e5284deeea397a625d
Ola Sh2 <shittu.babs@gmail.com>
2022-06-14 04:42:15 +0000

    Delete commented out code.

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,6 +1,6 @@
 /* exported toObject */
 // Define a function named toObject that takes an array of two elements as an argument.
-// The function should return an object with the first element as the key and the second element as the value.
+// The function should return a new Object with the first element as the key and the second element as the value.
 //    define an empty object to a variable named newObject
 //    assign the second element of the array to newObject[firstElement]
 //    return the newObject
