a6488526ffc4f5b6c33fcd5bbfc7764c0c0a6702
Ola Sh2 <shittu.babs@gmail.com>
2022-06-21 02:21:46 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,7 @@
+console.log('Hello World');
+
+console.log(document.querySelector('h1'));
+console.log('$explanation ', document.querySelector('#explanation'));
+console.log('$hint ', document.querySelector('.hint'));
+console.log('$paragraphs ', document.querySelectorAll('p'));
+console.log('$links ', document.querySelectorAll('.example-link'));
